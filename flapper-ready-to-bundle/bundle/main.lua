b15ae530adb5c55d954b49cb01835a4c433800e7
Naexys <nicolasblaison@yahoo.fr>
2026-01-10 00:20:27 +0100

    Update main.lua to add 3D setting and conditional draw
    Added love.graphics.set3D(false) at the top of main.lua and a love.load function. Updated love.draw to only print 'Hello World!' if the screen is not 'bottom'. Updated the bundled zip file accordingly.

--- flapper-ready-to-bundle/bundle/main.lua
+++ flapper-ready-to-bundle/bundle/main.lua
@@ -1,6 +1,15 @@
+
+love.graphics.set3D(false)
+
+function love.load()
+    local caca = 0
+end
+
 function love.draw(screen)
-    local width, height = love.graphics.getDimensions(screen)
-    love.graphics.print("Hello World!", width / 2, height / 2)
+    if screen ~= "bottom" then
+        local width, height = love.graphics.getDimensions(screen)
+        love.graphics.print("Hello World!", width / 2, height / 2)
+    end
 end
 
 function love.gamepadpressed(joystick, button)
