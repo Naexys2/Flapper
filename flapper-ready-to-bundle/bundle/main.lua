c15009065b26635c9b49bb628b68c0a7c927f180
Naexys <nicolasblaison@yahoo.fr>
2026-01-10 00:00:46 +0100

    Center text and add gamepad quit in love.draw
    love.draw now centers 'Hello World!' using screen dimensions. Added love.gamepadpressed to quit the application when any gamepad button is pressed.

--- flapper-ready-to-bundle/bundle/main.lua
+++ flapper-ready-to-bundle/bundle/main.lua
@@ -1,3 +1,8 @@
-function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+function love.draw(screen)
+    local width, height = love.graphics.getDimensions(screen)
+    love.graphics.print("Hello World!", width / 2, height / 2)
+end
+
+function love.gamepadpressed(joystick, button)
+    love.event.quit()
 end
